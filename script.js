865173cb5feb655df59e20c42d4b129779fda863
ideyou <desenvolvimento.ideyou@gmail.com>
2026-05-25 22:09:50 -0300

    Atualiza o layout e adiciona animações de galáxia ao fundo

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 function toggleTheme() {
   document.body.classList.toggle("dark-mode");
 }
+
+
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 60
+        },
+
+        color: {
+            value: "#ffffff"
+        },
+
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 0.3
+        },
+
+        size: {
+            value: 2
+        },
+
+        move: {
+            enable: true,
+            speed: 1
+        },
+
+        line_linked: {
+            enable: false
+        }
+    }
+});
