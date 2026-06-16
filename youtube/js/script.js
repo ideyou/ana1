d81a2967ed15a6f4d6515378949d99eaed7e4acc
ideyou <desenvolvimento.ideyou@gmail.com>
2026-06-15 22:08:27 -0300

    backup

---
+++ youtube/js/script.js
@@ -0,0 +1,15 @@
+alert('javascript puxado de fora');
+
+//VARIAVEIS
+let nome = "Dimitri";
+const pi = 3.14059;
+
+//funções
+function saudacao(nome){
+
+   return "Olá, " + nome;
+}
+alert(saudacao("ANA"));
+function trocarcor(){
+   document.body.style.backgroundColor = "lightblue" 
+}
